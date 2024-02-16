0ca0dd71764ba3ea4cadbefc948634381850ca7d
Jogndev <jogn.profm@gmail.com>
2024-02-16 14:46:48 -0300

    Crear const en script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const botonEncriptar = document.querySelector(".boton-encriptar");
+const botonDesncriptar = document.querySelector(".boton-desencriptar");
+const muneco = document.querySelector(".muneco");
+const contenedor = document.querySelector(".contenedor-bajoMuneco");
+const resultado = document.querySelector("");
