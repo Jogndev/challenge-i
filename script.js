f82a2b776fab1d69b6d5faea4dbedff4131899f2
Jogndev <jogn.profm@gmail.com>
2024-02-21 20:17:21 -0300

    Edición js y html

--- script.js
+++ script.js
@@ -2,51 +2,90 @@
 
 let botonEncriptar = document.getElementById("botonEncriptar");
 let botonDesencriptar = document.getElementById("botonDesencriptar");
+let botonCopiar = document.getElementById("botonCopiar");
 let contenedorMuneco = getElementById("contenedorMuneco");
-let resultado = document.getElementById("textoResultado");
+let textoResultado = document.getElementById("textoResultado");
 let mensaje = document.getElementById("mensaje");
+
+// Botones //
 
 botonEncriptar.addEventListener("click", encriptar);
 
 botonDesencriptar.addEventListener("click", desencriptar);
 
-function encriptarTexto (texto) {
-    let listaClave = [
-        ["a", "ai"],
-        ["e", "enter"],
-        ["i", "imes"],
-        ["o", "ober"],
-        ["u", "ufat"]
-    ];
-    
-    texto = texto.toLowerCase();
-    
-    for (let i = 0; i < listaClave.length; i++) {
-        if (texto.includes(listaClave[i][0])) {
-            texto = texto.replaceAll(listaClave[i][0], listaClave[i][1]);
-        }
+botonCopiar.addEventListener("click", copiar = () => {
+        let contenido = document.getElementById("textoResultado").textContent;
+        navigator.clipboard.writeText(contenido).then(() => {
+            alert("texto copiado al portapapeles");
+        }).catch((err) => {
+            console.error('error al copiar el texto', err);
+        });
+    });
+
+// Función encriptar //
+
+function encriptarTexto (str) {
+    var reemplazo = {
+        a:"ai",
+        e:"enter",
+        i:"imes",
+        o:"ober",
+        u:"ufat"
+    };
+    str = str.replace(/a|e|i|o|u/g, function(coincidencia) 
+    {
+        return reemplazo[coincidencia];
+    });
+    console.log(str);
+    textoResultado.textContent = str;
+    if (textoResultado.innerText.length > 0) {
+        ocultarAdelante();
+    } else {
+        mostrarAdelante();
     }
+}
 
-    return texto;
-};
+// Función desencriptar //
 
-function desencriptarTexto(texto) {
-    let listaClave = [
-        ["ai", "a"],
-        ["enter", "e"],
-        ["imes", "i"],
-        ["ober", "o"],
-        ["ufat", "u"]
-    ];
-    texto = texto.toLowerCase();
-
-    for (let i = 0; i < listaClave.length; i++) {
-        let clave = listaClave[i];
-        let regex = new RegExp("\\b" + clave[0] + "\\b", "g");
-        texto = texto.replace(regex, clave[1]);
+function desencriptarTexto(str) {
+    var reemplazo = {
+        ai:"a",
+        enter:"e",
+        imes:"i",
+        ober:"o",
+        ufat:"u"
+    };
+    str = str.replace(/ai|enter|imes|ober|ufat/g, function(coincidencia){
+        return reemplazo[coincidencia];
+    });
+    console.log(str);
+    textoResultado.textContent = str;
+    if (textoResultado.innerText.length > 0) {
+        ocultarAdelante();
+    } else {
+        mostrarAdelante();
     }
-    return texto;
-};
+}
+
+// Función revisar mayúsculas //
+
+function revisarMayusculas(str) {
+    const conMayusculas = /[A-ZÁÉÍÓÚ]/;
+    const conTildes = /[ÁÉÍÓÚáéíóú]/;
+    return conMayusculas.test(str) || conTildes.test(str);
+}
+
+// Limpieza de la caja de texto mensaje //
+
+mensaje.addEventListener("click", function() {
+    mensaje.value = "";
+});
+
+// Ocultar contenedorMuneco //
+
+
+
+// Mostrar contenedorMuneco //
 
 function encriptar () {
     ocultarAdelante();
@@ -60,23 +99,4 @@ function desencriptar() {
     ocultarAdelante();
 }
 
-function recuperarTexto() {
-    let cajaTexto = document.querySelector(".texto");
-    return cajaTexto.value;
-}
-
-function ocultarAdelante() {
-    contenedorMuneco.classList.add("ocultar");
-    contenedorBajoMuneco.classList.add("ocultar");
-}
-
-let botonCopiar = document.querySelector(".botonCopiar");
-    botonCopiar.addEventListener("click", copiar = () => {
-        let contenido = document.querySelector(".textoResultado").textContent;
-        navigator.clipboard.writeText(contenido).then(() => {
-            alert("texto copiado al portapapeles");
-        }).catch((err) => {
-            console.error('error al copiar el texto', err);
-        });
-    });
     
