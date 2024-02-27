84b8373e70c010b6aeb8050f2693efd3e746eb09
Jogndev <jogn.profm@gmail.com>
2024-02-27 13:18:26 -0300

    Agrega nueva función desencriptar

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function botonEncriptar() {
     mensaje.style.backgroundImage = "none";
 }
 
+function botonDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value  = "";
+}
+
 // Función encriptar //
 function encriptar(stringEncriptada) {
     let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
@@ -27,7 +33,17 @@ function encriptar(stringEncriptada) {
 
 
 // Función desencriptar //
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
 
+    for(let i = 0; i < matrizCodigo.length; i++ ) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+}
 
 
     
