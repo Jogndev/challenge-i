c225bfce9dbb2e58e64807b943de82ade5563992
Jogndev <jogn.profm@gmail.com>
2024-02-27 12:59:01 -0300

    Reemplazo archivo js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // Definición botones y texto entrada y salida //
 
-let botonEncriptar = document.getElementById("botonEncriptar");
-let botonDesencriptar = document.getElementById("botonDesencriptar");
-let botonCopiar = document.getElementById("botonCopiar");
-let contenedorMuneco = getElementById("contenedorMuneco");
-let textoResultado = document.getElementById("textoResultado");
-let mensaje = document.getElementById("mensaje");
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensajeEncriptado");
 
 // Botones //
 
-botonEncriptar.addEventListener("click", encriptar);
-
-botonDesencriptar.addEventListener("click", desencriptar);
+function botonEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+}
 
 botonCopiar.addEventListener("click", copiar = () => {
         let contenido = document.getElementById("textoResultado").textContent;
@@ -23,63 +20,33 @@ botonCopiar.addEventListener("click", copiar = () => {
     });
 
 // Función encriptar //
-
-function encriptarTexto (str) {
-    var reemplazo = {
-        a:"ai",
-        e:"enter",
-        i:"imes",
-        o:"ober",
-        u:"ufat"
-    };
-    str = str.replace(/a|e|i|o|u/g, function(coincidencia) 
-    {
-        return reemplazo[coincidencia];
-    });
-    console.log(str);
-    textoResultado.textContent = str;
-    if (textoResultado.innerText.length > 0) {
-        ocultarAdelante();
-    } else {
-        mostrarAdelante();
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptado = stringEncriptado.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++ ) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
     }
 }
+
 
 // Función desencriptar //
 
-function desencriptarTexto(str) {
-    var reemplazo = {
-        ai:"a",
-        enter:"e",
-        imes:"i",
-        ober:"o",
-        ufat:"u"
-    };
-    str = str.replace(/ai|enter|imes|ober|ufat/g, function(coincidencia){
-        return reemplazo[coincidencia];
-    });
-    console.log(str);
-    textoResultado.textContent = str;
-    if (textoResultado.innerText.length > 0) {
-        ocultarAdelante();
-    } else {
-        mostrarAdelante();
-    }
-}
+
 
 // Función revisar mayúsculas //
 
-function revisarMayusculas(str) {
-    const conMayusculas = /[A-ZÁÉÍÓÚ]/;
-    const conTildes = /[ÁÉÍÓÚáéíóú]/;
-    return conMayusculas.test(str) || conTildes.test(str);
-}
+
 
 // Limpieza de la caja de texto mensaje //
 
-mensaje.addEventListener("click", function() {
-    mensaje.value = "";
-});
 
 // Ocultar contenedorMuneco //
 
@@ -87,16 +54,6 @@ mensaje.addEventListener("click", function() {
 
 // Mostrar contenedorMuneco //
 
-function encriptar () {
-    ocultarAdelante();
-    let cajaTexto = recuperarTexto();
-    resultado.textContent = encriptarTexto(cajaTexto);
-}
 
-function desencriptar() {
-    let cajaTexto = recuperarTexto();
-    resultado.textContent = desencriptarTexto(cajaTexto);
-    ocultarAdelante();
-}
 
     
